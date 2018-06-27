47a9249dcc4787475366ae1d59d425e56029a380
Volodymyr Yaremkiv <vv.yaremkiv@gmail.com>
2018-06-27 09:08:49 +0300

    add modifications

--- CalculateKeynessRelative.py
+++ CalculateKeynessRelative.py
@@ -52,9 +52,10 @@ def save_rang_result(path, counter, F_counter, f_counter, top_n=None):
             i += 1
 
 
-text_path = 'D:/Research/John Ronald Reuel Tolkien/LOTR.txt'
+text_path = 'D:/English base2cleaned&expanded_Metko/20,000_Leagues_Under_the_Sea-Jules_Verne.txt'
+# text_path = 'D:/English base2cleaned&expanded_Metko/A_Scandal_In_Bohemia-Conan_Doyle.txt'
 main_corpora_data_path = 'D:/Research/PythonProjects/CorpusData'
-corpora_name = 'English base2cleaned&expanded_Metko'
+corpora_name = 'English base2cleaned&expanded_Metko_withOtherTolkienTexts'
 useMinF = True
 minF = 10
 useMinf = False
@@ -81,21 +82,25 @@ if useMinf:
     F_filtered_counter = Counter({k: v for k, v in F_counter.items() if v / La >= minf})
 
 f_counter = Counter({k: v / La for k, v in F_filtered_counter.most_common()})
+f_sq_counter = Counter({k: v ** 2 for k, v in f_counter.most_common()})
 unique_words = {word for word in f_counter if word not in words_corpora}
 nc = n + 1
 Lc = sum_L + La
+print(La)
 add_text_words_stats = {}
 for key in f_counter:
     Fa = F_filtered_counter[key]
     fa = f_counter[key]
+    fq = f_sq_counter[key]
     if key in unique_words:
         print(key)
         Fc = Fa
         fc = fa / nc
-        fwc = fa / Lc
+        fwc = Fa / Lc
         ntc = 1
         fs = ((Fa ** 2) / (La ** 2)) / nc
         fws = ((Fa ** 2) / La) / Lc
+        sigma_c = math.sqrt((fq / nc) - (fc ** 2))
     else:
         Fc = words_corpora[key][0] + Fa
         fc = (words_corpora[key][1] * n + fa) / nc
@@ -103,10 +108,25 @@ for key in f_counter:
         ntc = words_corpora[key][3] + 1
         fs = (words_corpora[key][7] + ((Fa ** 2) / (La ** 2))) / nc
         fws = (words_corpora[key][6] + ((Fa ** 2) / La)) / Lc
-    sigma_c = math.sqrt(fs - (fc ** 2))
+        sigma_c = math.sqrt(fs - (fc ** 2))
     sigma_wc = math.sqrt(fws - (fwc ** 2))
     add_text_words_stats[key] = [Fc, fc, fwc, ntc, sigma_c, sigma_wc]
+# key = 'nastia'
+# # a = f_counter[key]-add_text_words_stats[key][1]
+# # print(a)
+# # b = add_text_words_stats[key][4]
+# # print(b)
+# # print(a/b)
+# # key = 'starosta'
+# # a = f_counter[key]-add_text_words_stats[key][1]
+# # print(a)
+# # b = add_text_words_stats[key][4]
+# # print(b)
 
+# c = (a**2)/nc
+# d = (a/nc)**2
+# print((a-a/nc)/math.sqrt(c-d))
+# print(a/b)
 rm1_counter = Counter({k: f * math.log(nc / add_text_words_stats[k][3], 10) for k, f in f_counter.items()})
 
 rm3_counter = Counter({k: (f - add_text_words_stats[k][1]) / add_text_words_stats[k][4]
@@ -121,6 +141,18 @@ rm5w_counter = Counter({k: ((f - add_text_words_stats[k][2]) / add_text_words_st
                            math.log(nc / add_text_words_stats[k][3]) for k, f in f_counter.items()
                         if f - add_text_words_stats[k][2] > 0.0})
 
+rm8_counter = Counter({k: f * ((f - add_text_words_stats[k][1]) / add_text_words_stats[k][4])
+                       for k, f in f_counter.items() if f - add_text_words_stats[k][1] > 0.0})
+rm8w_counter = Counter({k: f * ((f - add_text_words_stats[k][2]) / add_text_words_stats[k][5])
+                        for k, f in f_counter.items() if f - add_text_words_stats[k][2] > 0.0})
+
+rm9_counter = Counter({k: f * ((f - add_text_words_stats[k][1]) / add_text_words_stats[k][4]) *
+                          math.log(nc / add_text_words_stats[k][3])
+                       for k, f in f_counter.items() if f - add_text_words_stats[k][1] > 0.0})
+rm9w_counter = Counter({k: f * ((f - add_text_words_stats[k][2]) / add_text_words_stats[k][5]) *
+                          math.log(nc / add_text_words_stats[k][3])
+                        for k, f in f_counter.items() if f - add_text_words_stats[k][2] > 0.0})
+
 current_path = os.path.dirname(os.path.abspath(__file__))
 new_path = os.path.join(current_path, 'ResultRelativeMethod_'+corpora_name)
 if not os.path.exists(new_path):
@@ -130,5 +162,9 @@ save_rang_result(os.path.join(new_path, 'rm4.csv'), rm3_counter, F_filtered_coun
 save_rang_result(os.path.join(new_path, 'rm5.csv'), rm3w_counter, F_filtered_counter, f_counter, topN)
 save_rang_result(os.path.join(new_path, 'rm8.csv'), rm5_counter, F_filtered_counter, f_counter, topN)
 save_rang_result(os.path.join(new_path, 'rm9.csv'), rm5w_counter, F_filtered_counter, f_counter, topN)
+save_rang_result(os.path.join(new_path, 'rm10.csv'), rm8_counter, F_filtered_counter, f_counter, topN)
+save_rang_result(os.path.join(new_path, 'rm11.csv'), rm8w_counter, F_filtered_counter, f_counter, topN)
+save_rang_result(os.path.join(new_path, 'rm12.csv'), rm9_counter, F_filtered_counter, f_counter, topN)
+save_rang_result(os.path.join(new_path, 'rm13.csv'), rm9w_counter, F_filtered_counter, f_counter, topN)
 print('n={0}, sum L={1}'.format(nc, Lc))
 print("time elapsed", time.perf_counter() - start_time, "s")
--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-test_string = '-'
-print(test_string.strip('-\''))
+import glob
+from chardet.universaldetector import UniversalDetector
+
+detector = UniversalDetector()
+for filename in glob.glob('D:/English base2cleaned&expanded_Metko/*.txt'):
+
+    detector.reset()
+    for line in open(filename, 'rb'):
+        detector.feed(line)
+        if detector.done:
+            break
+    detector.close()
+    if detector.result['encoding'] != 'UTF-8-SIG':
+        print(filename.ljust(60))
+        print(detector.result)
